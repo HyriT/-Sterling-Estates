4c84ee9cbaa38d5c5913f8c4f4b993b8a7bbf6c0
HyriT <163197566+HyriT@users.noreply.github.com>
2025-09-08 15:23:05 +0200

    Db config and module import

---
+++ src/Config/db_config.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    console.log("Mongo URI:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB successfully!');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/server.js
@@ -0,0 +1,55 @@
+
+import dotenv from 'dotenv'; 
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import http from 'http'; 
+import { Server } from 'socket.io'; 
+import connectDB from './Config/db_config.js';
+
+dotenv.config(); 
+connectDB();
+
+const app = express();
+const port = 5000;
+
+// Create HTTP server for WebSocket
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  },
+});
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  credentials: true
+}));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Conection with WebSocket
+io.on('connection', (socket) => {
+  console.log('Admin connected:', socket.id);
+
+  socket.join('admins');
+
+  socket.on('disconnect', () => {
+    console.log('Admin disconnected:', socket.id);
+  });
+});
+
+// Start server
+server.listen(port, () => {
+  console.log(` Server is running on http://localhost:${port}`);
+});
+
