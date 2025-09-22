bca0cae932f032286b286a9a8d755a03fc4bf62b
HyriT <163197566+HyriT@users.noreply.github.com>
2025-09-22 16:55:28 +0200

    Routes file changes

--- src/Routes/listingRoutes.js
+++ src/Routes/listingRoutes.js
@@ -3,9 +3,9 @@ const router =express.Router();
 import {createListing,deleteListing,updateListing,getListing,getListings,getListingsFiltered, getAllListings ,getProperties} from '../Controllers/listingController.js'
 import {verifyAdmin} from '../Middleware/authMiddleware.js';
 
-router.post('/create',verifyAdmin,createListing)
+router.post('/create',createListing)
 router.post('/delete/:id',verifyAdmin,deleteListing)
-router.post('/update/:id',verifyAdmin,updateListing)
+router.post('/update/:id',updateListing)
 router.get('/get/:id',verifyAdmin,getListing)
 router.get('/get',getListings);
 router.get('/filtered',getListingsFiltered);
